77973a9733c1b912057ffb34d0e877a763cfe16f
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-20 18:10:19 +0700

    https://oj.luyencode.net/problem/CB07

---
+++ CB07.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+double n;
+const double PI=3.14;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    cout<<fixed<<setprecision(3)<<n*2*PI<<" "<<n*n*PI;
+}
