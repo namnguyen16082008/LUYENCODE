bf3212aee6d6bb58f07d567c32d414c5df7fb919
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-20 21:02:15 +0700

    https://oj.luyencode.net/problem/CB04

---
+++ CB04.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+float a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    cout<<a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b;
+    return 0;
+}
