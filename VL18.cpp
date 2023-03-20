e143414b8d247ea549a0e24c24ca038aeee0d47d
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-20 18:23:57 +0700

    https://oj.luyencode.net/problem/VL18

---
+++ VL18.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>s;
+    for (ll i=0;i<s.size()/2;i++)
+        swap(s[i],s[s.size()-i-1]);
+}
