a40f228a07a586a2c9efc76ea23b4488d54c223f
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-24 18:58:07 +0700

    https://oj.luyencode.net/problem/AVG3NUM

---
+++ AVG3NUM.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,x;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>x;
+    cout<<x*3-a-b;
+}
