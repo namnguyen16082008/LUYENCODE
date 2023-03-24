7e55c4795d75693d6ae01244cf55fff3baefa405
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-24 19:14:23 +0700

    https://oj.luyencode.net/problem/PTIT051

---
+++ PTIT051.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+char k=char(92);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    cout<<"users setClock "<<k<<b<<k<<a<<k<<c;
+}
