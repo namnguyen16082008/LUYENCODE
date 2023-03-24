3e98995ae3038b0502c15aeed1d98e743e802729
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-24 19:15:31 +0700

    https://oj.luyencode.net/problem/FUNCTION

---
+++ FUNCTION.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c,x;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c>>x;
+    cout<<a*x*x+b*x+c;
+}
