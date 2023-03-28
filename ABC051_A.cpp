cb99fe1fd126113a98d1ab30e055bd2eb9abb34a
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-28 18:43:27 +0700

    https://oj.luyencode.net/problem/ABC051_A

---
+++ ABC051_A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    getline (cin,s);
+    s[5]=' ';s[13]=' ';
+    cout<<s;
+}
