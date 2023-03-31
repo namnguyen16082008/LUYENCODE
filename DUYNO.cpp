ae64458416fccc48f9f1a8c398c910763a7a2f3f
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-31 18:37:18 +0700

    https://oj.luyencode.net/problem/DUYNO

---
+++ DUYNO.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	while (getline (cin,s)) (s[0]==s[s.size()-1]) ?cout<<"YES"<<endl :cout<<"NO"<<endl;
+}
