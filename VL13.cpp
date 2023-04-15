cf69fc79ae2c5e226a0d8628ff964ec3647df3b0
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-15 21:32:45 +0700

    https://oj.luyencode.net/problem/VL13

---
+++ VL13.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n,ans=0;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=sqrt(n);i++)
+        if (n%i==0)
+            (i*i==n) ? ans+=i : ans+=(i+n/i);
+    (ans==2*n) ? cout<<"YES" : cout<<"NO";
+}
