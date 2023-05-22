db358ebd2bb212462b0c1eea508f566d4aae507d
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-22 20:20:47 +0700

    https://oj.luyencode.net/problem/NUMTRANS

---
+++ NUMTRANS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+const ll N=105;
+ll n;
+string a[N];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++) cin>>a[i];
+    sort(a+1,a+n+1,greater<string>());
+    for (ll i=1;i<=n;i++) cout<<a[i];
+}
