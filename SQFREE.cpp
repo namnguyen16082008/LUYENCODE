478f51eb0027e303a5eba50b61d0a9711385f272
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-25 21:47:25 +0700

    https://oj.luyencode.net/problem/SQFREE

--- SQFREE.cpp
+++ SQFREE.cpp
@@ -4,16 +4,6 @@
 using namespace std;
 
 ll n,x;
-ll Pr(ll n){
-    if (n<=3) return n>1;
-    if (n%2==0 || n%3==0) return 0;
-    ll i=5;
-    while (i*i<=n){
-        if (n%i==0 || n%(i+2)==0) return 0;
-        i+=6;
-    }
-    return 1;
-}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -27,15 +17,9 @@ int main(){
             n/=i;
         }
     if (n>1){
-        if (Pr(n)==1){
-            cout<<"NO";
-            return 0;
-        }
-        else{
-            x=(ll)sqrt(n);
-            (x*x==n) ? cout<<"YES" : cout<<"NO";
-            return 0;
-        }
+        x=(ll)sqrt(n);
+        (x*x==n) ? cout<<"YES" : cout<<"NO";
+        return 0;
     }
     cout<<"NO";
 }
