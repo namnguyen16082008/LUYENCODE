870f4bbd1dd689da8af907d7cefae109d4d546f8
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-30 23:46:44 +0700

    https://oj.luyencode.net/problem/DEMSO0

--- DEMSO0.cpp
+++ DEMSO0.cpp
@@ -8,12 +8,9 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n;
-    for (ll i=1;i<=n;i++){
-        x=i;
-        while (x%5==0){
-            ans++;
-            x/=5;
-        }
+    while (n>0){
+        ans+=n/5;
+        n/=5;
     }
     cout<<ans;
 }
