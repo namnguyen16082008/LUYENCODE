b12a8c1e6726501ceab5978f989dc5e069e9c458
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-19 23:07:05 +0700

    https://oj.luyencode.net/problem/NPALIN1

---
+++ NPALIN1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n,a[20]={0,9,9,90,90,900,900,9000,9000,90000,90000,900000,900000,9000000,9000000,90000000};
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (cin>>n) cout<<a[n]<<endl;
+}
+
