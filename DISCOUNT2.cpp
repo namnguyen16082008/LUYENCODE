44d4bc92e0758f9949a362e74b554f66a70a3bcd
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-20 17:29:06 +0700

    https://oj.luyencode.net/problem/DISCOUNT2

---
+++ DISCOUNT2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+const ll N=100005;
+ll n,ans=0,a[N];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++) cin>>a[i];
+    sort(a+1,a+n+1,greater<ll>());
+    for (ll i=n/4+1;i<=n;i++) ans+=a[i];
+    cout<<ans;
+}
+
