06f9a5c4ec17d44d9d6e3db7be6812f080c16c30
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-24 23:21:51 +0700

    https://oj.luyencode.net/problem/XEPSO1

---
+++ XEPSO1.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll t,n;
+void Solve(){
+    cin>>n;
+    if (n==2) cout<<1<<" "<<1<<endl;
+    else if (n==3) cout<<7<<" "<<7<<endl;
+    else if (n==4) cout<<4<<" "<<11<<endl;
+    else if (n==5) cout<<2<<" "<<71<<endl;
+    else if (n==6) cout<<6<<" "<<111<<endl;
+    else if (n==7) cout<<8<<" "<<711<<endl;
+    else if (n==8) cout<<10<<" "<<1111<<endl;
+    else if (n==9) cout<<18<<" "<<7111<<endl;
+    else if (n==10) cout<<22<<" "<<11111<<endl;
+    else if (n==11) cout<<20<<" "<<71111<<endl;
+    else if (n==12) cout<<28<<" "<<111111<<endl;
+    else if (n==13) cout<<68<<" "<<711111<<endl;
+    else if (n==14) cout<<88<<" "<<1111111<<endl;
+    else if (n==15) cout<<108<<" "<<7111111<<endl;
+    else if (n==16) cout<<188<<" "<<11111111<<endl;
+    else if (n==17) cout<<200<<" "<<71111111<<endl;
+    else if (n==18) cout<<208<<" "<<111111111<<endl;
+    else if (n==19) cout<<288<<" "<<711111111<<endl;
+    else if (n==20) cout<<688<<" "<<1111111111<<endl;
+    else if (n==21) cout<<888<<" "<<7111111111<<endl;
+    else if (n==22) cout<<1088<<" "<<11111111111<<endl;
+    else if (n==23) cout<<1888<<" "<<71111111111<<endl;
+    else if (n==24) cout<<2008<<" "<<111111111111<<endl;
+    else if (n==25) cout<<2088<<" "<<711111111111<<endl;
+    else if (n==26) cout<<2888<<" "<<1111111111111<<endl;
+    else if (n==27) cout<<6888<<" "<<7111111111111<<endl;
+    else if (n==28) cout<<8888<<" "<<11111111111111<<endl;
+    else if (n==29) cout<<10888<<" "<<71111111111111<<endl;
+    else if (n==30) cout<<18888<<" "<<111111111111111<<endl;
+    else if (n==31) cout<<20088<<" "<<711111111111111<<endl;
+    else if (n==32) cout<<20888<<" "<<1111111111111111<<endl;
+    else if (n==33) cout<<28888<<" "<<7111111111111111<<endl;
+    else if (n==34) cout<<68888<<" "<<11111111111111111<<endl;
+    else if (n==35) cout<<88888<<" "<<71111111111111111<<endl;
+    else if (n==36) cout<<108888<<" "<<111111111111111111<<endl;
+    else if (n==37) cout<<188888<<" "<<711111111111111111<<endl;
+    else if (n==38) cout<<200888<<" "<<1111111111111111111<<endl;
+    else if (n==39) cout<<208888<<" "<<7111111111111111111<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while (t--) Solve();
+}
