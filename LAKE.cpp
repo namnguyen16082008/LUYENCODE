eece30f1710f945c8daad7387f657f681cc1bac5
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-25 01:48:25 +0700

    https://oj.luyencode.net/problem/LAKE

--- LAKE.cpp
+++ LAKE.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-ll n,m,k,a[101][101],x[101][101],d=0,z=0;
-void Try(ll i, ll j){
+const ll N=100;
+ll n,m,k,ans=0,d=0,a[N+5][N+5],x[N+5][N+5],o1[4]={-1,0,1,0},o2[4]={0,-1,0,1};
+void DFS(ll i, ll j){
 	x[i][j]=0;
-	z++;
-	d=max(d,z);
-	if (a[i-1][j]==0 && x[i-1][j]==1 && i>1) Try(i-1,j);
-	if (a[i+1][j]==0 && x[i+1][j]==1 && i<n) Try(i+1,j);
-	if (a[i][j-1]==0 && x[i][j-1]==1 && j>1) Try(i,j-1);
-	if (a[i][j+1]==0 && x[i][j+1]==1 && j<m) Try(i,j+1);
-	return;
+	d++;
+	ans=max(ans,d);
+	for (ll k=0;k<4;k++){
+        ll i1=i+o1[k],j1=j+o2[k];
+        if (a[i1][j1]==0 && x[i1][j1]==1 && i1>=1 && i1<=n && j1>=1 && j1<=m) DFS(i1,j1);
+	}
 }
 int main(){
     ios_base::sync_with_stdio(false);
@@ -31,8 +31,8 @@ int main(){
 	for (ll i=1;i<=n;i++)
 		for (ll j=1;j<=m;j++)
 			if (x[i][j]==1 && a[i][j]==0){
-				z=0;
-				Try(i,j);
+				d=0;
+				DFS(i,j);
 			}
-	cout<<d;
+	cout<<ans;
 }
