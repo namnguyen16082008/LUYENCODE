5d47f8c6c4575c0951973e76172136b5ee6386fb
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-25 00:17:35 +0700

    https://oj.luyencode.net/problem/PROJECT

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -3,30 +3,18 @@
 
 using namespace std;
 
-const ll N=25;
-ll n,s,ans=0,c[N+5],p[N+5],a[N+5];
-void Check(){
-    ll o=0;
-    for (ll i=1;i<=n;i++)
-        o+=a[i]*p[i];
-    ans=max(ans,o);
-}
-void Try(ll i, ll o){
-    for (ll j=0;j<=1;j++){
-        a[i]=j;
-        o+=c[i]*j;
-        if (o>s) return;
-        if (i<n) Try(i+1,o);
-        else Check();
-        o-=c[i]*j;
-    }
-}
+const ll N=25,S=30000;
+ll n,s,p,ans=0,c[N+5],x[S+5]={};
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n>>s;
     for (ll i=1;i<=n;i++) cin>>c[i];
-    for (ll i=1;i<=n;i++) cin>>p[i];
-    Try(1,0);
+    for (ll i=1;i<=n;i++){
+        cin>>p;
+        for (ll j=s;j>=c[i];j--)
+            if (j==c[i] || x[j-c[i]]!=0) x[j]=max(x[j],x[j-c[i]]+p);
+    }
+    for (ll i=1;i<=s;i++) ans=max(ans,x[i]);
     cout<<ans;
 }
