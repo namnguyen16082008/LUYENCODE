4a4da25214d9778dff3d10c06b10ab1cbd82c542
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-07-05 14:15:26 +0700

    https://oj.luyencode.net/problem/dk06

---
+++ dk06.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+double a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    if (a==0 && b==0) cout<<"WOW";
+    else if (a==0 && b!=0) cout<<"NO";
+    else cout<<fixed<<setprecision(2)<<-b/a;
+}
